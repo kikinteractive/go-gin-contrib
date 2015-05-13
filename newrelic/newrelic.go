2539f728f1398d4275139b5cbee0bd778a5190a8
David Bolshoy <bolshoy@gmail.com>
2015-05-13 17:12:38 +0300

    Changed to use rounds/gorelic package, which permits adding custom metrics. Added http status metrics.

--- newrelic/newrelic.go
+++ newrelic/newrelic.go
@@ -1,29 +1,52 @@
+// New Relic metrics reporter middleware.
+
 package newrelic
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	metrics "github.com/yvasiyarov/go-metrics"
 	"github.com/yvasiyarov/gorelic"
 )
 
 var agent *gorelic.Agent
 
-func NewRelic(license string, appname string, verbose bool) gin.HandlerFunc {
-	agent = gorelic.NewAgent()
-	agent.NewrelicLicense = license
+// InitAgent creates and inits a newrelic agent.
+func InitAgent(license, appName, hostName string, verbose bool) error {
 
-	agent.HTTPTimer = metrics.NewTimer()
+	// Sanity checks.
+	switch {
+	case license == "":
+		return fmt.Errorf("empty newrelic license")
+	case appName == "":
+		return fmt.Errorf("empty newrelic app name")
+	case hostName == "":
+		return fmt.Errorf("empty newrelic hostname")
+	}
+
+	agent = gorelic.NewAgent()
+
+	// Init agent identification parameters (company license, application name and hostname).
+	agent.NewrelicLicense = license
+	agent.AgentGUID = appName
+	agent.NewrelicName = hostName
+
+	// Init http metric collectors.
 	agent.CollectHTTPStat = true
+	agent.CollectHTTPStatuses = true
 	agent.Verbose = verbose
 
-	agent.NewrelicName = appname
 	agent.Run()
+	return nil
+}
 
-	return func(c *gin.Context) {
-		startTime := time.Now()
-		c.Next()
+// Handler is a gin middleware handler. It wraps the default handler and updates metric timers and counters.
+func Handler(c *gin.Context) {
+	startTime := time.Now()
+	c.Next()
+	if agent != nil {
 		agent.HTTPTimer.UpdateSince(startTime)
+		agent.HTTPStatusCounters[c.Writer.Status()].Inc(1)
 	}
 }
